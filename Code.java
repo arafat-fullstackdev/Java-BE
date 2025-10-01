aad223c095fa57e79a212e251e8abe8850a884b6
Arafat Patwary <farabipsk@gmail.com>
2025-10-01 19:40:26 +0600

    Create multiple object

--- Code.java
+++ Code.java
@@ -1,6 +1,25 @@
 public class Code{
+   int speed = 100;
+   int invention = 1995;
+   String Node = "Rayan Dal";
+
      public static void main(String[] args){
         String value = "Java";
-        System.out.println("Hello " + value);;
+        
+        System.out.println("Hello " + value);
+
+        
+     Code car_obj = new Code();
+     System.out.println(car_obj.speed);
+
+     Code OOP = new Code();
+     Code Script = new Code();
+     
+     System.out.println(OOP.invention);
+     System.out.println(Script.Node);
      }
+
+     //make object
+
 };
+
