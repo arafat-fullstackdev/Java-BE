0797ff796a69c473f39181a817f388eb8c38baf4
Arafat Patwary <farabipsk@gmail.com>
2025-10-03 18:37:16 +0600

    Make all Project

--- CalorieCalculator/Src/calorie/sixty_minCal.java
+++ CalorieCalculator/Src/calorie/sixty_minCal.java
@@ -1,5 +1,16 @@
 package CalorieCalculator.Src.calorie;
 
 public class sixty_minCal {
+
+    public static void main(String[] args){
+        Main JilMill = new Main();
+        Main Mill = new Main();
+        Main Max = new Main();
+
+            System.out.println("The amount of calories you need per day is: " + 99 * JilMill.sixty_minCal);
+            System.out.println("The amount of calories you need per day is: " + 109 * Mill.sixty_minCal);
+            System.out.println("The amount of calories you need per day is: " + 119 * Max.sixty_minCal);
+
+    }
     
 }
