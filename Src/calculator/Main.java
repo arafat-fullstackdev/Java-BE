d0b2cdea46539bfb4dfcee4dc13f253bb5809d60
Arafat Patwary <farabipsk@gmail.com>
2025-10-17 08:51:07 +0600

    Create MBR

---
+++ Src/calculator/Main.java
@@ -0,0 +1,14 @@
+package Src.calculator;
+
+
+public class Main(){
+  double men_val1;
+  double men_val2;
+  double men_val13;
+  double men_val4;
+
+  double women_val1;
+  double women_val2;
+  double women_val3;
+  double women_val4;
+}
---
+++ Src/calculator/MenBMR.java
@@ -0,0 +1,13 @@
+package Src.calculator;
+
+import java.util.Scanner;
+public class MenBMR {
+    public static void main(String[] args){
+      Scanner object = new Scanner(System.in);
+      Main Andrew = new Main(); // Create Person Object
+      System.out.println("Enter your Weight in kilogram: ");
+      double user_weight = object.nextDouble();
+      System.out.println("Enter your Weight in kilogram: ");
+      double user_height = object.nextDouble();
+    }
+}
